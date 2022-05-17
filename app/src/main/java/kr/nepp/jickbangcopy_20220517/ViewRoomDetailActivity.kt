3a2b58a0045e6941c558acfcf3d6ab0611fd9743
임백산 <turuc@naver.com>
2022-05-17 17:16:56 +0900

    직방 상세 페이지 완료

--- app/src/main/java/kr/nepp/jickbangcopy_20220517/ViewRoomDetailActivity.kt
+++ app/src/main/java/kr/nepp/jickbangcopy_20220517/ViewRoomDetailActivity.kt
@@ -2,10 +2,20 @@ package kr.nepp.jickbangcopy_20220517
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
+import kotlinx.android.synthetic.main.activity_view_room_detail.txtPrice
+import kotlinx.android.synthetic.main.room_list_item.*
+import kr.nepp.jickbangcopy_20220517.datas.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as RoomData
+
+        txtPrice.text = roomData.getFormattedPrice()
+        txtFloor.text = roomData.getFormattedFloor()
+        txtDesc.text = roomData.description
     }
 }
